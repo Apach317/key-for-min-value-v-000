d4e3c56c5a23ef64854f91dc17b2385a732f8f3f
Ron Lynch <lynch.ronald1@gmail.com>
2018-09-04 02:50:40 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,12 +5,18 @@ def key_for_min_value(name_hash)
   min_n = nil
   min_v = nil
   name_hash.each do |key, value|
-    if age == 1
-      return name
-    elsif age == 10
-      return name
-    elsif age == nil
-      return nil
+    if min_n == nil || value < min_n
+      min_n = value
+      min_v = key
     end
   end
+  min_v
 end
+      #return name
+    #elsif age == 10
+      #return name
+    #elsif age == nil
+      #return nil
+    #end
+  #end
+#end
